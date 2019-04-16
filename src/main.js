acdb9edebc1dddec65472891f272ce4c827c8cca
A.N <spidernet12@gmail.com>
2019-04-16 22:19:22 +0300

    Update main.js
    referenced necessary libs

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import BootstrapVue from 'bootstrap-vue';
+import UUID from 'vue-uuid';
+import router from './router';
+import store from './store';
+import vueMoment from 'vue-moment';
 
-Vue.config.productionTip = false
+import '../custom.scss'
+
+Vue.config.productionTip = false;
+
+Vue.use(vueMoment);
+Vue.use(UUID);
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
