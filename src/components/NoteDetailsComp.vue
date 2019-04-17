8eca6a9e634986a2729338cbf514e2d6f772bb85
A.N <spidernet12@gmail.com>
2019-04-17 15:37:08 +0300

    Update NoteDetailsComp.vue
    Migrated to Vuex to dispatch actions instead of using local storage logic directly.

--- src/components/NoteDetailsComp.vue
+++ src/components/NoteDetailsComp.vue
@@ -1,127 +1,117 @@
 <template>
-    <div>
-        <b-container v-if="!viewMode" class="mb-5 header-margin">
-            <b-form-input class="mb-2" placeholder="Title" v-model="note.title" type="text"></b-form-input>
-            <b-form-textarea
-                v-model="note.content"
-                placeholder="Content"
-                rows="10"
-                max-rows="100"
-            ></b-form-textarea>
-        </b-container>
-        <b-container v-else class="mb-5 header-margin">
-            <h1>{{note.title}}</h1>
-            <div v-html="parsed()"></div>
-        </b-container>
-        <b-navbar fixed="bottom" type="light" variant="dark">
-            <b-collapse id="nav-collapse" is-nav>
-                <span v-if="!this.newNote" class="small text-muted"> {{this.noteTimeSuffix()}} {{  this.noteTime() | moment("DD/MM/YYYY, h:mm a") }}</span>
-                <b-navbar-nav class="ml-auto">
-                    <b-button @click="deleteNote()" variant="danger" size="sm" class="mr-2">Del</b-button>
-                    <b-button v-if="viewMode" @click="toggleViewMode()" size="sm" class="m-0">Edit</b-button>
-                    <b-button v-if="!viewMode" @click="saveNote()" :disabled="note.title === '' || note.content === ''" variant="info" size="sm" class="m-0">Save</b-button>
-                </b-navbar-nav>
-            </b-collapse>
-        </b-navbar>
-    </div>
+  <div v-if="isLoading === 0">
+    <b-container v-if="!viewMode" class="mb-5 header-margin">
+      <b-form-input class="mb-2" placeholder="Title" v-model="note.title" type="text"></b-form-input>
+      <b-form-textarea v-model="note.content" placeholder="Content" rows="10" max-rows="100"></b-form-textarea>
+    </b-container>
+    <b-container v-else class="mb-5 header-margin">
+      <h1>{{note.title}}</h1>
+      <div v-html="parsed()"></div>
+    </b-container>
+    <b-navbar fixed="bottom" type="light" variant="dark">
+      <b-collapse id="nav-collapse" is-nav>
+        <span v-if="!this.newNote" class="small text-muted">{{ this.noteTime() }}</span>
+        <b-navbar-nav class="ml-auto">
+          <b-button @click="deleteNote()" variant="danger" size="sm" class="mr-2">Del</b-button>
+          <b-button v-if="viewMode" @click="toggleViewMode()" size="sm" class="m-0">Edit</b-button>
+          <b-button
+            v-if="!viewMode"
+            @click="saveNote()"
+            :disabled="note.title === '' || note.content === ''"
+            variant="info"
+            size="sm"
+            class="m-0"
+          >Save</b-button>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
-import markDown from 'marked';
+import markDown from "marked";
 
 export default {
-    name: 'NoteDetailsComp',
-    data: function(){
-        return {
-            tempNotes: [],
-            note: {
-                title: '',
-                content: '',
-                dateCreated: `${this.$moment().format("YYYYMMDDTHHmmss")}`,
-                dateModefied: null
-            },
-            isLoading: 1,
-            newNote: true,
-            saved: false,
-            viewMode: false
-        };
-    },
-  created: function() {
-
+  name: "NoteDetailsComp",
+  data: function() {
+    return {
+      currentNoteId: null,
+      note: {},
+      isLoading: 1,
+      newNote: true,
+      saved: false,
+      viewMode: false
+    };
   },
   methods: {
-    noteTimeSuffix: function(){
-        if(this.note.dateModefied === null){
-            return 'C ';
-        }
-        return 'M ';
+    noteTime: function() {
+      if (this.note.dateModefied === null) {
+        return `C ${this.$moment(this.note.dateCreated).format("DD/MM/YYYY, h:mm a")}`;
+      }
+      return `M ${this.$moment(this.note.dateModefied).format("DD/MM/YYYY, h:mm a")}`;
     },
-    noteTime: function(){
-        if(this.note.dateModefied === null){
-            return this.note.dateCreated;
-        }
-        return this.note.dateModefied;
-    },
-    saveNote: function(){
-        if(this.newNote){
-            this.tempNotes.push(this.note);
-            this.commitChanges(this.tempNotes);
-            this.newNote = false;
-        } else {
-            for(let i = 0; i < this.tempNotes.length; i++){
-                if(this.tempNotes[i].id === this.note.id){
-                    this.note.dateModefied = `${this.$moment().format("YYYYMMDDTHHmmss")}`;
-                    this.tempNotes[i] = this.note;
-                    this.commitChanges();
-                    break;
-                }
+    saveNote: function() {
+      if (this.newNote) {
+        this.$store.dispatch("insertNote", this.note).then((response)=>{
+            if(response){
+                this.newNote = false;
+                this.viewMode = true;
             }
-        }
-        this.viewMode = true;
-    },
-    deleteNote: function(){
-        for(let i = 0; i < this.tempNotes.length; i++){
-            if(this.tempNotes[i].id === this.note.id){
-                this.tempNotes.splice(i, 1);
-                this.commitChanges();
-                this.$router.push({name: "home"});
-                break;
+        });
+      } else {
+        this.note.dateModefied = `${this.$moment().format("YYYYMMDDTHHmmss")}`;
+        this.$store.dispatch("updateNote", this.note).then((response)=>{
+            if(response){
+                this.viewMode = true;
             }
-        }
+        });
+      }
     },
-    commitChanges: function(){
-        localStorage.setItem('notes', JSON.stringify(this.tempNotes));
+    deleteNote: function() {
+      this.$store.dispatch("deleteNote", this.currentNoteId).then((response)=>{
+          if(response){
+              this.$router.push({ name: "home" });
+          }
+      });
     },
-    parsed: function(){
-        return markDown(this.note.content);
+    getNote: function() {
+      return this.$store.getters.getNoteById(this.currentNoteId);
     },
-    toggleViewMode: function(){
-        this.viewMode = !this.viewMode;
+    parsed: function() {
+      return markDown(this.note.content);
+    },
+    toggleViewMode: function() {
+      this.viewMode = !this.viewMode;
+    }
+  },
+  computed: {
+    notes() {
+        return this.$store.state.notes.notes;
     }
   },
   mounted: function() {
-      this.tempNotes = JSON.parse(localStorage.getItem('notes'));
-      this.note.id = this.$route.params.id;
-        for(let i = 0; i < this.tempNotes.length; i++){
-            if(this.tempNotes[i].id === this.note.id){
-                this.note = this.tempNotes[i];
-                this.newNote = false;
-                this.viewMode = true;
-                break;
-            }
-        }
+    this.currentNoteId = this.$route.params.id;
+    const tempNote = this.getNote();
+    if (tempNote !== null) {
+      this.note = tempNote;
+      this.newNote = false;
+      this.viewMode = true;
+    } else {
+      this.note = {
+        id: this.currentNoteId,
+        title: "",
+        content: "",
+        dateCreated: `${this.$moment().format("YYYYMMDDTHHmmss")}`,
+        dateModefied: null
+      };
+    }
     this.isLoading = 0;
-  },
-  updated: function() {
-  },
-  destroyed: function() {
-
   }
-}
+};
 </script>
 
 <style scoped>
-    .header-margin{
-        margin-top: 5rem;
-    }
+.header-margin {
+  margin-top: 5rem;
+}
 </style>
