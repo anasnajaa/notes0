5c5e276250fe24f9ba64f5a74e10e7f33a033feb
A.N <spidernet12@gmail.com>
2019-04-18 08:54:14 +0300

    Update NoteCard.vue
    Added tags support to cards

--- src/components/NoteCard.vue
+++ src/components/NoteCard.vue
@@ -1,49 +1,55 @@
 <template>
-<div>
+  <div>
     <b-card @click="viewNote" class="card">
-        <b-card-title>{{note.title}}</b-card-title>
-        <b-card-text v-html="this.parsedNoteContent()">
-        </b-card-text>
-        <b-card-text class="small text-muted"> {{this.noteTimeSuffix()}} {{  this.noteTime() | moment("from", "now") }}</b-card-text>
+      <b-card-title>{{note.title}}</b-card-title>
+      <b-card-text v-html="this.parsedNoteContent()"></b-card-text>
+      <b-card-text class="small text-muted">{{ this.noteTime() }}</b-card-text>
+      <div v-if="note.tags.length > 0">
+          <b-badge class="mr-1" pill @click.stop="viewTag(tag)" :key="tag" v-for="tag in note.tags" variant="secondary">{{ tag }}</b-badge>
+      </div>
     </b-card>
-</div>
+  </div>
 </template>
 
 <script>
-import markDown from 'marked';
+import markDown from "marked";
 
 export default {
-    props: {
-        note: Object
+  props: {
+    note: Object
+  },
+  name: "noteCard",
+  methods: {
+    noteTime: function() {
+      if (this.note.dateModefied === null) {
+        return `C ${this.$moment(this.note.dateCreated).fromNow(true)}`;
+      }
+      return `M ${this.$moment(this.note.dateModefied).fromNow(true)}`;
     },
-    name: 'noteCard',
-    methods: {
-        noteTimeSuffix: function(){
-            if(this.note.dateModefied === null){
-                return 'C ';
-            }
-            return 'M ';
-        },
-        noteTime: function(){
-            if(this.note.dateModefied === null){
-                return this.note.dateCreated;
-            }
-            return this.note.dateModefied;
-        },
-        viewNote: function(){
-            this.$router.push({name: "noteDetails", params:{id: this.note.id}});
-        },
-        parsedNoteContent: function(){
-            return markDown(this.note.content)
-        }
+    viewNote: function() {
+      this.$router.push({ name: "noteDetails", params: { id: this.note.id } });
     },
-    mounted: function(){
+    viewTag: function(tagName){
+        this.$router.push({ name: "notesByTag", params: { name: tagName } });
+    },
+    parsedNoteContent: function() {
+      return markDown(this.note.content);
     }
-}
+  },
+  mounted: function() {}
+};
 </script>
 
 <style scoped >
 .card {
-    cursor: pointer;
+  cursor: pointer;
+}
+
+.card .card-body {
+    padding: .75rem;
+}
+
+.card .card-text {
+    margin: 0;
 }
 </style>
