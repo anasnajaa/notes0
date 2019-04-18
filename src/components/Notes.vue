1ff6c249e8800b8680dfc7e9cdb1ce5dbdde1284
A.N <spidernet12@gmail.com>
2019-04-18 08:53:41 +0300

    Added pagination support

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <b-container class="header-margin mb-5">
-      <b-card-group columns v-if="this.notes.length > 0">
-        <NoteCard :key="note.id" v-for="note in this.notes" :note="note"/>
+      <div class="overflow-auto">
+        <b-pagination-nav limit="12" size="sm" align="fill" :link-gen="linkGen" :number-of-pages="paginationPagesCount" use-router></b-pagination-nav>
+      </div>
+      <b-card-group columns v-if="this.notesFilteredByPage.length > 0">
+        <NoteCard :key="note.id" v-for="note in this.notesFilteredByPage" :note="note"/>
       </b-card-group>
       <b-alert v-else show class="text-center" variant="light">No notes. <br/>
         Powered by <a href="https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage">localStorage</a> and 
@@ -21,21 +24,49 @@ export default {
   },
   data: function(){
     return {
+      notesFilteredByPage: [],
+      notesPerPage: 12
     };
   },
-  props: {
-
-  },
-  created: function() {},
   mounted: function() {
+    this.filterByPage();
+  },
+  methods: {
+      linkGen(pageNum) {
+        return pageNum === 1 ? '?' : `?page=${pageNum}`
+      },
+      filterByPage: function(){
+        this.notesFilteredByPage = [];
+        const startingIndex = (this.currentPage - 1) * this.notesPerPage;
+        let endingIndex = startingIndex + this.notesPerPage;
+        endingIndex = endingIndex > this.notes.length ? this.notes.length : endingIndex;
+        for(let i = startingIndex; i < endingIndex; i++){
+          this.notesFilteredByPage.push(this.notes[i]);
+        }
+      }
   },
   computed: {
     notes() {
         return this.$store.state.notes.notes;
+    },
+    currentPage(){
+      return this.$route.query.page || 1;
+    },
+    paginationPagesCount(){
+      if(this.notes.length > 0){
+        return Math.ceil(this.notes.length / this.notesPerPage) || 1;
+      }
+      return 1
     }
   },
-  updated: function() {},
-  destroyed: function() {}
+  destroyed: function() {},
+    watch: {
+      $route: function(to) {
+          if(to.name === "home"){
+            this.filterByPage();
+          }
+      }
+  }
 }
 </script>
 
