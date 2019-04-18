8b22fdbc8b1251b24a1c43d441eae42f507555a0
A.N <spidernet12@gmail.com>
2019-04-18 09:30:45 +0300

    Service worker configs

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,18 @@
 module.exports = {
     // ...other vue-cli plugin options...
     pwa: {
-        // configure the workbox plugin
-        workboxPluginMode: 'InjectManifest',
-        workboxOptions: {
-            // swSrc is required in InjectManifest mode.
-            swSrc: 'public/service-worker.js',
-            // ...other Workbox options...
-        }
+      name: 'My App',
+      themeColor: '#4DBA87',
+      msTileColor: '#000000',
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: 'black',
+   
+      // configure the workbox plugin
+      workboxPluginMode: 'InjectManifest',
+      workboxOptions: {
+        // swSrc is required in InjectManifest mode.
+        swSrc: 'public/service-worker.js',
+        // ...other Workbox options...
+      }
     }
-}
+  }
